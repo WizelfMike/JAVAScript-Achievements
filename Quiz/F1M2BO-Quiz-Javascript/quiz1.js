27e7d6a608b3d45ca1923541d58e0761f1a987b5
WizelfMike <32264@ma-web.nl>
2021-01-06 15:17:02 +0100

    added the quiz folder instead of the normal repo

---
+++ Quiz/F1M2BO-Quiz-Javascript/quiz1.js
@@ -0,0 +1,73 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "LAM Pop Quiz",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2020/05/LAM-filmpje-Bad-Grapes_FCZ2864-CZFc2020-024.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "What are the oversized grapes made of?",
+				"answers": [{
+						"answer": "Stone",
+						"feedback": false
+					},
+					{
+						"answer": "Porcelain",
+						"feedback": false
+					},
+					{
+						"answer": "Gem Stone",
+						"feedback": true
+					},
+					{
+						"answer": "Quartz",
+						"feedback": false
+					},
+				]
+			},
+			{
+				"question": "LAM – What's the collection's theme?",
+				"answers": [{
+						"answer": "Street art",
+						"feedback": false
+					},
+					{
+						"answer": "Food art",
+						"feedback": true
+					},
+					{
+						"answer": "Colorfull art",
+						"feedback": false
+					},
+					{
+						"answer": "Kitsch art",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Lam can be found in what province?",
+				"answers": [{
+						"answer": "Brabant",
+						"feedback": false
+					},
+					{
+						"answer": "North Holland",
+						"feedback": false
+					},
+					{
+						"answer": "Groningen",
+						"feedback": false
+					},
+					{
+						"answer": "South Holland",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ Quiz/F1M2BO-Quiz-Javascript/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Trivia",
+        "imageURI": "https://expattutor.files.wordpress.com/2015/04/other-planets-galaxies.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "What's 9 + 10  ",
+				"answers": [{
+						"answer": "20",
+						"feedback": false
+					},
+					{
+						"answer": "21",
+						"feedback": false
+					},
+					{
+						"answer": "19",
+						"feedback": true
+					},
+					{
+						"answer": "22",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "65 / 43 =  ?",
+				"answers": [{
+						"answer": "1.62239910",
+						"feedback": false
+					},
+					{
+						"answer": "1.5116279",
+						"feedback": true
+					},
+					{
+						"answer": "1.753288574",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Who was the main character in the 2005 movie The Pacifier?",
+				"answers": [{
+						"answer": "Dwane *The Rock* Johnson",
+						"feedback": false
+					},
+					{
+						"answer": "Tom Cruise",
+						"feedback": false
+					},
+					{
+						"answer": "Vin Diesel",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
